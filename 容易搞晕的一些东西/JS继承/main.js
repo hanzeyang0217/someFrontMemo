11e26d325027b73f310bc56f5dad80d44ea55f29
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-01 07:22:38 +0900

    0229

--- 容易搞晕的一些东西/JS继承/main.js
+++ 容易搞晕的一些东西/JS继承/main.js
@@ -5,12 +5,22 @@
 // Person.prototype.sayName = function() {
 //   console.log(this.name);
 // };
-// const p1 = new Person(`yy`, 15);
-// const p2 = new Person(`yyy`, 12);
+// function 杨家将() {
+//   this.family = `杨家将`;
+//   Person.apply(this, arguments);
+// }
+// 杨家将.prototype.__proto__ = Person.prototype;
+
+// const p1 = new 杨家将(`yy`, 15);
+// const p2 = new 杨家将(`yyy`, 12);
 // console.log(p1.name); //yy
 // p1.sayName(); //yy
 // p1.sayName.call(p1); //yy
 // p1.sayName.call(p2); //yyy
+// 杨家将.prototype.sayName = function() {
+//   console.log(`we are 杨家将`);
+// };
+// p1.sayName(); //we are 杨家将
 // p1.sayName = () => {
 //   console.log(`tt`);
 // };
@@ -28,13 +38,28 @@ class Person {
     console.log(this.name);
   }
 }
+Person.prototype.sayName = function() {
+  console.log(this.name);
+};
+class 杨家将 extends Person {
+  constructor(name, age, waihao) {
+    super(name, age);
+    this.family = `杨家将`;
+    this.waihao = waihao;
+  }
+  sayName() {
+    super.sayName();
+    console.log(`we are 杨家将`);
+  }
+}
 
-const p1 = new Person(`yy`, 15);
-const p2 = new Person(`yyy`, 12);
+const p1 = new 杨家将(`yy`, 15, `六郎`);
+const p2 = new 杨家将(`yyy`, 12, `九郎`);
 console.log(p1.name); //yy
-p1.sayName(); //yy
-p1.sayName.call(p1); //yy
-p1.sayName.call(p2); //yyy
+console.log(p1.waihao); //六郎
+p1.sayName(); //yy we are 杨家将
+p1.sayName.call(p1); //yy we are 杨家将
+p1.sayName.call(p2); //yyy we are 杨家将
 p1.sayName = () => {
   console.log(`tt`);
 };
