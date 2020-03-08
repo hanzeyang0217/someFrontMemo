00769e292f74e34f295f4017a793886bc981fc70
Hanzeyang <hanzeyang0217@gmail.com>
2020-03-08 19:14:38 +0900

    0308

--- MVC/MVC入门/src/section3/section3.js
+++
@@ -1,6 +0,0 @@
-import "./section3.css";
-const square = document.querySelector(`.square`);
-
-square.onclick = e => {
-  square.classList.toggle(`active`);
-};
--- MVC/MVC入门/src/section4/section4.js
+++
@@ -1,9 +0,0 @@
-import "./section4.css";
-const circle = document.querySelector(`.circle`);
-
-circle.onmouseenter = e => {
-  circle.classList.toggle(`active`);
-};
-circle.onmouseout = e => {
-  circle.classList.toggle(`active`);
-};
